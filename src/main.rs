ab95c7e18dcb1cfe5f5237feec7f4def8e6335e1
Julian Theis <julian.theis@posteo.de>
2021-02-20 17:10:43 +0100

    Reply to start/stop messages

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use std::env;
 use telegram_bot::{
+    prelude::*,
     Api,
     Error,
+    Message,
     MessageKind,
-    UpdateKind,
+    UpdateKind
 };
 use tokio_stream::StreamExt;
 
@@ -12,20 +14,20 @@ async fn main() -> Result<(), Error> {
     let token: String = env::var("DAILYKAENGURU_TOKEN").expect("Could not fetch DAILYKAENGURU_TOKEN environment variable");
     let api = Api::new(token);
 
-    handle_updates(&api).await
+    handle_updates(api).await
 }
 
-async fn handle_updates(api: &Api) -> Result<(), Error> {
+async fn handle_updates(api: Api) -> Result<(), Error> {
     let mut stream = api.stream();
 
     while let Some(update) = stream.next().await {
-	let update = update.expect("Received broken update");
+	let update = update?;
 
 	if let UpdateKind::Message(message) = update.kind {
 	    if let MessageKind::Text { ref data, .. } = message.kind {
 		match data.as_str() {
-		    "/start" => println!("{} started the bot", message.from.username.unwrap_or("Someone".to_string())),
-		    "/stop" => println!("{} stopeed the bot", message.from.username.unwrap_or("Someone".to_string())),
+		    "/start" => start_cmd(&api, message).await,
+		    "/stop" => stop_cmd(&api, message).await,
 		    _ => ()
 		}
 	    }
@@ -33,4 +35,15 @@ async fn handle_updates(api: &Api) -> Result<(), Error> {
     }
 
     Ok(())
+}
+
+async fn start_cmd(api: &Api, message: Message) {
+    let chat = message.chat;
+    api.send(chat.text("Hallo!")).await;
+}
+
+async fn stop_cmd(api: &Api, message: Message) {
+    let chat = message.chat;
+    api.send(chat.text("Ciao!")).await;
+    api.send(chat.leave()).await;
 }
