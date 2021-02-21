c737d5132a287f13bdf87a0cd8f1f752a3fa6723
Julian Theis <julian.theis@posteo.de>
2021-02-21 16:56:31 +0100

    Move bot functionality into own module

---
+++ src/bot.rs
@@ -0,0 +1,65 @@
+use log::{
+    debug,
+    info
+};
+use telegram_bot::{
+    prelude::*,
+    Api,
+    Error,
+    Message,
+    MessageChat,
+    MessageKind,
+    UpdateKind
+};
+use time::OffsetDateTime;
+use tokio_stream::StreamExt;
+
+
+pub async fn handle_updates(token: String) -> Result<(), Error> {
+    let api = Api::new(token);
+    let mut stream = api.stream();
+
+    while let Some(update) = stream.next().await {
+	let update = update?;
+
+	if let UpdateKind::Message(message) = update.kind {
+	    if let MessageKind::Text { ref data, .. } = message.kind {
+		match data.as_str() {
+		    "/start" => start_cmd(&api, message).await?,
+		    "/stop" => stop_cmd(&api, message).await?,
+		    _ => ()
+		}
+	    }
+	}
+    }
+
+    Ok(())
+}
+
+
+async fn start_cmd(api: &Api, message: Message) -> Result<(), Error> {
+    let username = message.from.username.unwrap_or("people".to_string());
+    let chat = message.chat;
+
+    info!("Starting delivery to {} in chat {}", username, chat.id());
+    api.send(chat.text("Hallo!")).await?;
+
+    Ok(())
+}
+
+
+async fn stop_cmd(api: &Api, message: Message) -> Result<(), Error> {
+    let username = message.from.username.unwrap_or("people".to_string());
+    let chat = message.chat;
+
+    info!("Stopping delivery to {} in chat {}", username, chat.id());
+    api.send(chat.text("Ciao!")).await?;
+
+    match chat {
+	MessageChat::Private(_) => debug!("Cannot leave private chat"),
+	_ => api.send(chat.leave()).await?
+    }
+
+    Ok(())
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,16 @@
+mod bot;
+
 use getopts::Options;
 use log::{
     debug,
     info
 };
 use std::env;
-use telegram_bot::{
-    prelude::*,
-    Api,
-    Error,
-    Message,
-    MessageChat,
-    MessageKind,
-    UpdateKind
-};
 use time::OffsetDateTime;
-use tokio_stream::StreamExt;
 
 
 #[tokio::main]
-async fn main() -> Result<(), Error> {
+async fn main() {
     env_logger::init();
 
     let args: Vec<String> = env::args().collect();
@@ -30,7 +22,7 @@ async fn main() -> Result<(), Error> {
 	if matches.opt_present("h") {
 	    let brief = format!("Usage: {} [OPTIONS]", &args[0]);
 	    println!("{}", opts.usage(&brief));
-	    return Ok(());
+	    return;
 	}
 	
 	let data_path: String = env::var("DAILYKAENGURU_DATA").expect("Could not fetch DAILYKAENGURU_DATA environment variable");
@@ -39,60 +31,9 @@ async fn main() -> Result<(), Error> {
 	    let comic = download_comic(datetime, "https://img.zeit.de/administratives/kaenguru-comics", "original").await;
 	} else {
 	    let token: String = env::var("DAILYKAENGURU_TOKEN").expect("Could not fetch DAILYKAENGURU_TOKEN environment variable");
-	    return handle_updates(token).await;
+	    bot::handle_updates(token).await;
 	}
     }
-    
-    Ok(())
-}
-
-
-async fn handle_updates(token: String) -> Result<(), Error> {
-    let api = Api::new(token);
-    let mut stream = api.stream();
-
-    while let Some(update) = stream.next().await {
-	let update = update?;
-
-	if let UpdateKind::Message(message) = update.kind {
-	    if let MessageKind::Text { ref data, .. } = message.kind {
-		match data.as_str() {
-		    "/start" => start_cmd(&api, message).await?,
-		    "/stop" => stop_cmd(&api, message).await?,
-		    _ => ()
-		}
-	    }
-	}
-    }
-
-    Ok(())
-}
-
-
-async fn start_cmd(api: &Api, message: Message) -> Result<(), Error> {
-    let username = message.from.username.unwrap_or("people".to_string());
-    let chat = message.chat;
-
-    info!("Starting delivery to {} in chat {}", username, chat.id());
-    api.send(chat.text("Hallo!")).await?;
-
-    Ok(())
-}
-
-
-async fn stop_cmd(api: &Api, message: Message) -> Result<(), Error> {
-    let username = message.from.username.unwrap_or("people".to_string());
-    let chat = message.chat;
-
-    info!("Stopping delivery to {} in chat {}", username, chat.id());
-    api.send(chat.text("Ciao!")).await?;
-
-    match chat {
-	MessageChat::Private(_) => debug!("Cannot leave private chat"),
-	_ => api.send(chat.leave()).await?
-    }
-
-    Ok(())
 }
 
 
