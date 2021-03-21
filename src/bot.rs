ccdedf6288a15c4e0ecac9977ec89a565e169952
Julian Theis <julian.theis@posteo.de>
2021-03-21 23:26:07 +0100

    Remember chat ids instead of full chat structs

--- src/bot.rs
+++ src/bot.rs
@@ -1,47 +1,50 @@
-use crate::download::{
-    self,
-    DownloadConfig
-};
+use crate::download::{self, DownloadConfig};
 
-use chrono::{
-    Duration,
-    Local,
-    NaiveTime
-};
+use chrono::{Duration, Local, NaiveTime};
 use std::collections::HashSet;
 use std::sync::{Arc, Mutex};
-use telegram_bot::{prelude::*, Api, Error, Message, MessageChat, MessageKind, UpdateKind, InputFileUpload};
+use telegram_bot::{
+    prelude::*, Api, ChatId, Error, InputFileUpload, Message, MessageChat, MessageKind, SendPhoto,
+    UpdateKind,
+};
 use tokio::time;
 use tokio_stream::StreamExt;
 
-type ChatCache = Arc<Mutex<HashSet<MessageChat>>>;
+type ChatCache = Arc<Mutex<HashSet<ChatId>>>;
 
-async fn deliver_comic(api: &Api, chat_cache: ChatCache, delivery_time: NaiveTime, config: &DownloadConfig) {
+async fn deliver_comic(
+    api: &Api,
+    chat_cache: ChatCache,
+    delivery_time: NaiveTime,
+    config: &DownloadConfig,
+) {
     loop {
-	let sleep_duration = time::Duration::from_secs(seconds_remaining(delivery_time));
-	time::sleep(sleep_duration).await;
+        let sleep_duration = time::Duration::from_secs(seconds_remaining(delivery_time));
+        time::sleep(sleep_duration).await;
 
-	let comic = match download::get_comic(Local::now(), &config).await {
-	    Ok(content) => InputFileUpload::with_data(content, "känguru.jpg"),
-	    Err(error) => {
-		log::warn!("Could not get comic: {}", error);
-		continue;
-	    }
-	};
-	
-	let chats = match chat_cache.lock() {
-	    Ok(chats) => chats.clone(),
-	    Err(error) => {
-		log::warn!("Cannot lock chat cache: {}", error);
-		continue;
-	    }
-	};
+        let comic = match download::get_comic(Local::now(), &config).await {
+            Ok(content) => InputFileUpload::with_data(content, "känguru.jpg"),
+            Err(error) => {
+                log::warn!("Could not get comic: {}", error);
+                continue;
+            }
+        };
 
-	for chat in chats.iter() {
-	    let _ = api.send(chat.photo(&comic))
-		.await
-		.map_err(|error| log::warn!("Could not deliver comic: {}", error));
-	}
+        let chats = match chat_cache.lock() {
+            Ok(chats) => chats.clone(),
+            Err(error) => {
+                log::warn!("Cannot lock chat cache: {}", error);
+                continue;
+            }
+        };
+
+        for chat in chats.iter() {
+            let send_photo = SendPhoto::new(chat, &comic);
+            let _ = api
+                .send(send_photo)
+                .await
+                .map_err(|error| log::warn!("Could not deliver comic: {}", error));
+        }
     }
 }
 
@@ -50,13 +53,17 @@ fn seconds_remaining(delivery_time: NaiveTime) -> u64 {
     let duration = delivery_datetime.signed_duration_since(Local::now());
 
     if duration.num_seconds() < 0 {
-	(duration + Duration::hours(24)).num_seconds() as u64
+        (duration + Duration::hours(24)).num_seconds() as u64
     } else {
-	duration.num_seconds() as u64
+        duration.num_seconds() as u64
     }
 }
 
-pub async fn handle_updates(token: String, config: DownloadConfig, delivery_time: NaiveTime) -> Result<(), Error> {
+pub async fn handle_updates(
+    token: String,
+    config: DownloadConfig,
+    delivery_time: NaiveTime,
+) -> Result<(), Error> {
     let chat_cache: ChatCache = Arc::new(Mutex::new(HashSet::new()));
     let api = Api::new(token);
     let mut stream = api.stream();
@@ -64,21 +71,21 @@ pub async fn handle_updates(token: String, config: DownloadConfig, delivery_time
     let api_copy = api.clone();
     let chat_cache_copy = chat_cache.clone();
     tokio::spawn(async move {
-	deliver_comic(&api_copy, chat_cache_copy, delivery_time, &config).await;
+        deliver_comic(&api_copy, chat_cache_copy, delivery_time, &config).await;
     });
 
     while let Some(update) = stream.next().await {
-	let update = update?;
+        let update = update?;
 
-	if let UpdateKind::Message(message) = update.kind {
-	    if let MessageKind::Text { ref data, .. } = message.kind {
-		match data.as_str() {
-		    "/start" => start_cmd(&api, chat_cache.clone(), message).await?,
-		    "/stop" => stop_cmd(&api, chat_cache.clone(), message).await?,
-		    _ => (),
-		}
-	    }
-	}
+        if let UpdateKind::Message(message) = update.kind {
+            if let MessageKind::Text { ref data, .. } = message.kind {
+                match data.as_str() {
+                    "/start" => start_cmd(&api, chat_cache.clone(), message).await?,
+                    "/stop" => stop_cmd(&api, chat_cache.clone(), message).await?,
+                    _ => (),
+                }
+            }
+        }
     }
 
     Ok(())
@@ -89,19 +96,19 @@ async fn start_cmd(api: &Api, chat_cache: ChatCache, message: Message) -> Result
     let chat = message.chat;
 
     match chat_cache.lock() {
-	Ok(mut chats) => {
-	    if chats.insert(chat.clone()) {
-		log::info!("Starting delivery to {} in chat {}", username, chat.id());
-		api.send(chat.text("Hallo!")).await?;
-	    } else {
-		log::info!("Already delivering to {} in chat {}", username, chat.id());
-		api.send(chat.text("Schon unterwegs!")).await?;
-	    }
-	}
-	Err(error) => {
-	    log::warn!("Could not lock chat cache: {}", error);
-	    api.send(chat.text("Razupaltuff")).await?;
-	}
+        Ok(mut chats) => {
+            if chats.insert(chat.id()) {
+                log::info!("Starting delivery to {} in chat {}", username, chat.id());
+                api.send(chat.text("Hallo!")).await?;
+            } else {
+                log::info!("Already delivering to {} in chat {}", username, chat.id());
+                api.send(chat.text("Schon unterwegs!")).await?;
+            }
+        }
+        Err(error) => {
+            log::warn!("Could not lock chat cache: {}", error);
+            api.send(chat.text("Razupaltuff")).await?;
+        }
     }
 
     Ok(())
@@ -113,12 +120,12 @@ async fn stop_cmd(api: &Api, chat_cache: ChatCache, message: Message) -> Result<
 
     log::info!("Stopping delivery to {} in chat {}", username, chat.id());
     if let Ok(mut chats) = chat_cache.lock() {
-	chats.remove(&chat);
+        chats.remove(&chat.id());
     }
 
     match chat {
-	MessageChat::Private(_) => log::debug!("Cannot leave private chat"),
-	_ => api.send(chat.leave()).await?,
+        MessageChat::Private(_) => log::debug!("Cannot leave private chat"),
+        _ => api.send(chat.leave()).await?,
     }
 
     api.send(chat.text("Ciao!")).await?;
