4493cf73addb9776916d73acd226d355727261e3
Julian Theis <julian.theis@posteo.de>
2021-11-21 20:13:53 +0100

    Use rust from mozilla overlay

--- shell.nix
+++ shell.nix
@@ -1,18 +1,22 @@
-with import <nixpkgs> {};
+let
+  moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
+  nixpkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
+in
+  with nixpkgs;
+  mkShell {
+    nativeBuildInputs = with nixpkgs; [
+      pkgconfig
+      rust-analyzer
+      nixpkgs.latest.rustChannels.stable.rust
+    ];
 
-mkShell {
-  nativeBuildInputs = [
-    pkgconfig
-    rust-analyzer
-    rustup
-  ];
-  buildInputs = [
-    cacert
-    openssl
-  ];
-
-  RUST_LOG = "info";
-  DAILYKAENGURU_DATA = "data/";
-  DAILYKAENGURU_DELIVERY = "10:30";
-  DAILYKAENGURU_TOKEN = "";
-}
+    buildInputs = [
+      cacert
+      openssl
+    ];
+  
+    RUST_LOG = "info";
+    DAILYKAENGURU_DATA = "data/";
+    DAILYKAENGURU_DELIVERY = "10:30";
+    DAILYKAENGURU_TOKEN = "";
+  }
