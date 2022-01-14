275bb89bdd960d1b9e55619269e9bd1d85975219
Julian Theis <julian.theis@posteo.de>
2022-01-14 16:42:24 +0100

    Set up nix shell for cross-compilation

--- shell.nix
+++ shell.nix
@@ -1,20 +1,26 @@
 let
   moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
-  nixpkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
+  nixpkgs = import <nixpkgs> {
+    overlays = [ moz_overlay ];
+    crossSystem = { config = "aarch64-unknown-linux-gnu"; };
+  };
 in
   with nixpkgs;
   mkShell {
-    nativeBuildInputs = with nixpkgs; [
+    nativeBuildInputs = with pkgsBuildHost; [
+      latest.rustChannels.stable.rust
       pkgconfig
       rust-analyzer
-      nixpkgs.latest.rustChannels.stable.rust
+      stdenv.cc
     ];
 
-    buildInputs = [
+    buildInputs = with pkgsHostTarget; [
       cacert
       openssl
     ];
-  
+
+    CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER = "${stdenv.cc.targetPrefix}cc";
+
     RUST_LOG = "info";
     DAILYKAENGURU_DATA = "data/";
     DAILYKAENGURU_DELIVERY = "10:30";
